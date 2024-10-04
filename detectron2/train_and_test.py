f61ef3d9feea54f59534cb72092a6a47ead53462
nOctaveLay <wjddkdml1124@naver.com>
2024-10-04 21:16:44 +0900

    split train_set and validation_set - make function : split_train_and_val() ref: https://stages.ai/competitions/325/board/community/post/2833

--- detectron2/train_and_test.py
+++ detectron2/train_and_test.py
@@ -3,6 +3,8 @@
 import random
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
+import json
 from tqdm import tqdm
 import copy
 import torch
@@ -23,19 +25,73 @@ import detectron2.data.transforms as T
 
 # config 불러오기
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml'))
+cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/retinanet_R_101_FPN_3x.yaml'))
 
 # train, test 둘 다 해당하는 내용 수정
 cfg.DATA_DIR = '/data/ephemeral/home/dataset/'
-cfg.OUTPUT_DIR = 'output/seed_test1'
+cfg.OUTPUT_DIR = 'output/retinanet_R_101_FPN_3x_epoch15000'
+cfg.OUTPUT_EVAL_DIR = '/output_eval/retinanet_R_101_FPN_3x_epoch15000'
 cfg.DATALOADER.NUM_WOREKRS = 2
-
 
 # Register Dataset
 # train / test를 구분할 필요는 없다. (method에서 해결)
+
+# 1. train_set, val_set split
+def split_train_and_val():
+
+    # 전처리
+    # COCO 형식의 train.json 로드
+    with open(os.path.join(cfg.DATA_DIR,'train.json'), 'r') as f:
+        coco_data = json.load(f)
+    
+    # image_id 별로 annotations를 묶기
+    image_annotations = dict()
+    for anno in coco_data['annotations']:
+        image_id = anno['image_id']
+        if image_id not in image_annotations:
+            image_annotations[image_id] = []
+        image_annotations[image_id].append(anno)
+        
+    # 이미지 리스트 추출
+    image_ids = list(image_annotations.keys())
+
+    # train, val split
+    train_idx, val_idx = train_test_split(image_ids, test_size=0.25, random_state=42)
+    train_image_ids = [image_ids[i] for i in train_idx]
+    val_image_ids = [image_ids[i] for i in val_idx]
+    
+    # Train과 Val에 해당하는 annotations 필터링
+    train_annotations = [anno for image_id in train_image_ids for anno in image_annotations[image_id]]
+    val_annotations = [anno for image_id in val_image_ids for anno in image_annotations[image_id]]
+    
+    # Train과 Val에 해당하는 이미지 필터링
+    train_images = [img for img in coco_data['images'] if img['id'] in train_image_ids]
+    val_images = [img for img in coco_data['images'] if img['id'] in val_image_ids]
+
+    # train fold json 생성
+    train_data = coco_data.copy()
+    train_data['annotations'] = train_annotations
+    train_data['images'] = train_images
+    with open(os.path.join(cfg.DATA_DIR,'split_train.json'), 'w') as f:
+        json.dump(train_data, f)
+    
+    # validation fold json 생성
+    val_data = coco_data.copy()
+    val_data['annotations'] = val_annotations
+    val_data['images'] = val_images
+    with open(os.path.join(cfg.DATA_DIR, 'split_val.json'), 'w') as f:
+        json.dump(val_data, f)
+
+split_train_and_val()
+
 try:
     # name, metadata, jsonfile, image_root
-    register_coco_instances('coco_trash_train', {}, os.path.join(cfg.DATA_DIR, 'train.json'), cfg.DATA_DIR)
+    register_coco_instances('coco_trash_train', {}, os.path.join(cfg.DATA_DIR, 'split_train.json'), cfg.DATA_DIR)
+except AssertionError:
+    pass
+
+try:
+    register_coco_instances('coco_trash_val', {}, os.path.join(cfg.DATA_DIR, 'split_val.json'), cfg.DATA_DIR)
 except AssertionError:
     pass
 
@@ -51,9 +107,10 @@ MetadataCatalog.get('coco_trash_train').thing_classes = ["General trash", "Paper
 
 # train config 수정하기
 cfg.DATASETS.TRAIN = ('coco_trash_train',)
+cfg.DATASETS.TEST = ('coco_trash_val',)
 cfg.DATALOADER.NUM_WOREKRS = 2
 
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml')
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-Detection/retinanet_R_101_FPN_3x.yaml')
 
 cfg.SOLVER.IMS_PER_BATCH = 4
 cfg.SOLVER.BASE_LR = 0.001
@@ -63,10 +120,10 @@ cfg.SOLVER.GAMMA = 0.005
 cfg.SOLVER.CHECKPOINT_PERIOD = 3000
 
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+cfg.MODEL.RETINANET.NUM_CLASSES = 10
+
 
 # test config 수정하기
-cfg.DATASETS.TEST = ('coco_trash_test',)
 cfg.TEST.EVAL_PERIOD = 3000
 
 
@@ -86,7 +143,6 @@ seed_everything()
 
 
 # mapper - input data를 어떤 형식으로 return할지 (따라서 augmnentation 등 데이터 전처리 포함 됨)
-
 def trainMapper(dataset_dict):
     dataset_dict = copy.deepcopy(dataset_dict)
     image = utils.read_image(dataset_dict['file_name'], format='BGR')
@@ -135,8 +191,8 @@ class MyTrainer(DefaultTrainer):
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
-            os.makedirs('./output_eval/retina', exist_ok = True)
-            output_folder = './output_eval/retina'
+            os.makedirs(cfg.OUTPUT_EVAL_DIR, exist_ok = True)
+            output_folder = cfg.OUTPUT_EVAL_DIR
             
         return COCOEvaluator(dataset_name, cfg, False, output_folder)
 
@@ -150,8 +206,9 @@ def train():
 
 
 def test():
-    # config 수정하기
 
+    # config 수정하기
+    cfg.DATASETS.TEST = ('coco_trash_test',)
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, 'model_final.pth')
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3
 
@@ -187,13 +244,17 @@ def test():
 
     return prediction_strings, file_names
 
-def submission(prediction_strings, file_names, save_file_name = 'submission_det2.csv'):
+def submission(prediction_strings, file_names, save_file_name = 'submission_det.csv'):
     submission = pd.DataFrame()
     submission['PredictionString'] = prediction_strings
     submission['image_id'] = file_names
     submission.to_csv(os.path.join(cfg.OUTPUT_DIR, save_file_name), index=None)
 
 if __name__ == "__main__":
+    # print(cfg.MODEL)
+    split_train_and_val()
+    cfg.DATASETS.TRAIN = ('coco_trash_train',)
+    cfg.DATASETS.TEST = ('coco_trash_val',)
     train()
-    prediction_strings, file_names = test()
-    submission(prediction_strings, file_names, save_file_name='test.csv')
+    # prediction_strings, file_names = test()
+    # submission(prediction_strings, file_names)

01935365f670db2fc98645d246db4fd1579b17a8
Taehan Kim <taehan5479@gmail.com>
2024-10-10 19:19:03 +0900

    feat : img_size, batch_size, eta test #26

--- Co-DETR/projects/configs/co_dino/co_dino_5scale_lsj_swin_large_1x_coco.py
+++ Co-DETR/projects/configs/co_dino/co_dino_5scale_lsj_swin_large_1x_coco.py
@@ -28,7 +28,7 @@ model = dict(
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-image_size = (1280, 1280)
+image_size = (512, 512)
 # load_pipeline = [
 #     dict(type='LoadImageFromFile'),
 #     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),

525c3b3f202817bcd72c3fdd1db60282e708c30f
Two-Silver <kgbhms2043@gmail.com>
2024-10-21 19:35:09 +0900

    feat : co-dino + mosaic Mosaic 증강 #77

---
+++ Co-DETR/projects/configs/co_dino/co_dino_5scale_lsj_swin_large_1x_coco_mosaic.py
@@ -0,0 +1,154 @@
+_base_ = [
+    'co_dino_5scale_lsj_r50_1x_coco.py'
+]
+load_from = '/data/ephemeral/home/taehan/level2-objectdetection-cv-18/Co-DETR/work_dirs/co_dino_5scale_lsj_swin_large_3x_coco/co_dino_5scale_lsj_swin_large_3x_coco.pth'
+pretrained = None
+
+# model settings
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformerV1',
+        embed_dim=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        out_indices=(0, 1, 2, 3),
+        window_size=12,
+        ape=False,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        use_checkpoint=True,
+        pretrained=pretrained),
+    neck=dict(in_channels=[192, 192*2, 192*4, 192*8]),
+    query_head=dict(
+        transformer=dict(
+            encoder=dict(
+                # number of layers that use checkpoint
+                with_cp=6))))
+
+# model settings Swin_tiny
+# model = dict(
+#     backbone=dict(
+#         _delete_=True,
+#         type='SwinTransformerV1',
+#         embed_dim=96,
+#         depths=[2, 2, 6, 2],
+#         num_heads=[3, 6, 12, 24],
+#         out_indices=(1, 2, 3),
+#         window_size=7,
+#         ape=False,
+#         drop_path_rate=0.2,
+#         patch_norm=True,
+#         use_checkpoint=False,
+#         pretrained=pretrained),
+#     neck=dict(in_channels=[96*2, 96*4, 96*8]),
+#     query_head=dict(
+#         transformer=dict(
+#             encoder=dict(
+#                 # number of layers that use checkpoint
+#                 with_cp=6))))
+
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+image_size = (1280, 1280)
+# load_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+#     dict(
+#         type='Resize',
+#         img_scale=image_size,
+#         ratio_range=(0.1, 2.0),
+#         multiscale_mode='range',
+#         keep_ratio=True),
+#     dict(
+#         type='RandomCrop',
+#         crop_type='absolute_range',
+#         crop_size=image_size,
+#         recompute_bbox=True,
+#         allow_negative_crop=True),
+#     dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+#     dict(type='RandomFlip', flip_ratio=0.5),
+#     dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+# ]
+# train_pipeline = [
+#     dict(type='CopyPaste', max_num_pasted=100),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+# ]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=image_size,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+# data_root = 'data/coco/'
+# dataset_type = 'CocoDataset'
+# data = dict(
+#     samples_per_gpu=1,
+#     workers_per_gpu=1,
+#     train=dict(
+#         type='MultiImageMixDataset',
+#         dataset=dict(
+#             type=dataset_type,
+#             ann_file=data_root + 'annotations/instances_train2017.json',
+#             img_prefix=data_root + 'train2017/',
+#             filter_empty_gt=False,
+#             pipeline=load_pipeline),
+#         pipeline=train_pipeline),
+#     val=dict(pipeline=test_pipeline),
+#     test=dict(pipeline=test_pipeline))
+
+# NOTE: LSJ with copy-paste requires segmentation annotations.
+# If your training data only contains bounding boxes, please use the following code:
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+
+    # Mosaic 증강을 추가
+    dict(
+        type='Mosaic',
+        img_scale=(1024, 1024),  # Mosaic 적용 후 리사이즈할 크기
+        center_ratio_range=(0.5, 1.5),  # 각 이미지 중심의 비율 범위 설정
+        bbox_clip_border=True, # 이미지 경계 밖 bbox 잘라내기
+        pad_val=114,  # 패딩 값 설정 (일반적으로 중성 회색 값)
+        prob=0.7  # 70% 확률로 Mosaic 적용 
+    ),
+
+    dict(
+        type='Resize',
+        img_scale=image_size,
+        ratio_range=(0.1, 2.0),
+        multiscale_mode='range',
+        keep_ratio=True),
+    # dict(
+    #     type='RandomCrop',
+    #     crop_type='absolute_range',
+    #     crop_size=image_size,
+    #     recompute_bbox=True,
+    #     allow_negative_crop=True),
+
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(filter_empty_gt=False, pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
